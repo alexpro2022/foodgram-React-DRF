f0c57cfdf4492c04e246ce646df0932cbdafdbbe
Alex Proskuriakov <alexpro1972@yandex.ru>
2022-10-06 19:56:46 +0300

    Removed pushing frontend

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -4,11 +4,11 @@ from django.core.exceptions import ValidationError
 
 
 def validate_username(username):
-    REGEX = r'[\w.@+-]+'
+    regex = r'[\w.@+-]+'
     if username == 'me':
         raise ValidationError(
             'Неверное имя пользователя: "me" зарезервировано')
-    invalid_symbols = ''.join(set(re.sub(REGEX, '', username)))
+    invalid_symbols = ''.join(set(re.sub(regex, '', username)))
     if invalid_symbols:
         raise ValidationError(
             f'Неверные символы {invalid_symbols} '
