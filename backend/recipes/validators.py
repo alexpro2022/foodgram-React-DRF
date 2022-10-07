338b164e216e8e66e38755943fc7a541b7cf1889
Alex Proskuriakov <alexpro1972@yandex.ru>
2022-10-07 22:29:49 +0300

    Readme file

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -4,8 +4,8 @@ from django.core.exceptions import ValidationError
 
 
 def validate_slug(slug):
-    REGEX = r'[-a-zA-Z0-9_]+'
-    invalid_symbols = ''.join(set(re.sub(REGEX, '', slug)))
+    regex = r'[-a-zA-Z0-9_]+'
+    invalid_symbols = ''.join(set(re.sub(regex, '', slug)))
     if invalid_symbols:
         raise ValidationError(
             f'Неверные символы {invalid_symbols} в слаге: "{slug}"')
@@ -13,8 +13,8 @@ def validate_slug(slug):
 
 
 def validate_color(hex_color):
-    REGEX = r'^[#A-Fa-f0-9]{7}$'
-    invalid_symbols = ''.join(set(re.sub(REGEX, '', hex_color)))
+    regex = r'^[#A-Fa-f0-9]{7}$'
+    invalid_symbols = ''.join(set(re.sub(regex, '', hex_color)))
     if invalid_symbols:
         raise ValidationError(
             f'Неверные символы {invalid_symbols} '
